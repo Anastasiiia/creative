3d2f35a7ca00e658cdc0cb6ea1a4fce412fc0169
Anastasiiia <nastya_graiv@ukr.net>
2015-04-02 13:03:04 +0300

    Update DominoMagicSquare.py

--- DominoMagicSquare.py
+++ DominoMagicSquare.py
@@ -9,12 +9,7 @@ def CheckBox(Box):
 	if sum([Box[i][i] for i in xrange(6)])!=13: return False
 	if sum([Box[5-i][i] for i in xrange(6)])!=13: return False
 	
-	for i in [0, 2, 4]:
-		for j in xrange(6):
-			for k in [0, 2, 4]:
-				for l in xrange(6):
-					if (Box[i][j]==Box[k][l] and Box[i+1][j]==Box[k+1][l]) or (Box[i][j]==Box[k+1][l] and Box[i+1][j]==Box[k][l]):
-						return False
+	
 	return True
 	
 	
